ec790c9783ef3ea8c755d0f289be306947baa9c9
Ravi-Guntur <166352916+Ravi-Guntur@users.noreply.github.com>
2025-09-03 13:35:39 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from CI/CD Pipeline project9 completed!"
+    return "Hello from CI/CD Pipeline project9 thanks completed!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
